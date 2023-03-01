24cf3daae27e6a7b3948608b62511705b51c2992
teebif <tebyforloni@gmail.com>
2023-02-28 23:23:20 -0300

    ProductoVendidoAct

---
+++ Controllers/ProductoVendidoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+
+
+namespace SistemaGestionWebApi
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoVendidoController : ControllerBase
+    {
+        [HttpGet("/ProductoVendido/{id}/")]  //Traer Productos Vendidos
+        public void TraerProductoVendido (int id)
+        {
+            ProductoVendidoCon.obtenerProductosVendidos(id);
+        }
+    }
+}
