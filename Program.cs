2b272a5f25fa54dd227b61991804cd3be478c0ec
teebif <tebyforloni@gmail.com>
2023-03-01 23:14:32 -0300

    Actualizacion casi final
    Editado de program.cs para el frontend

--- Program.cs
+++ Program.cs
@@ -5,11 +5,23 @@ using SistemaGestionWebApi;
                 // Add services to the container.
 
                 builder.Services.AddControllers();
+
+                builder.Services.AddCors(policy=>
+                {         
+                    policy.AddDefaultPolicy(options =>
+                    {          
+                        options.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                    });                
+                });  
+
+
                 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
                 builder.Services.AddEndpointsApiExplorer();
                 builder.Services.AddSwaggerGen();
 
                 var app = builder.Build();
+
+                app.UseCors();   
 
                 // Configure the HTTP request pipeline.
                 if (app.Environment.IsDevelopment())
